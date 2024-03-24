80b2ef7b762d305a3e90cfc1813373458bf578cb
harshy109 <141760030+harshy109@users.noreply.github.com>
2024-03-24 12:48:33 +0000

    Learned var const let

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="histesh@gmail.com" //used cuurently
+var accountPassword="12345" //never prefer
+accountCity="Jaipur" //not preferable
+let accountState
+
+// accountId=2
+
+/*prefer not to use var 
+because of issue in block scope and functional scope
+*/
+accountEmail="h@gmil.c"
+accountPassword="987"
+accountCity="Nagda"
+
+console.log(accountEmail)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
